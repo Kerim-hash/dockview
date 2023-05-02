3375b70b1bd303c8e71f4d189c8f9fd6fdb3ca53
Ruslanqq <Appleruslan7182@gmail.com>
2023-05-02 15:35:54 +0600

    init

---
+++ src/components/Box.jsx
@@ -0,0 +1,20 @@
+import { useLayoutEffect, useRef } from "react";
+
+export default function Box() {
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>1. Start editing to see some magic happen!</h2>
+      <p style={{ height: "300px" }}></p>
+      <h2>2. Start editing to see some magic happen!</h2>
+      <p style={{ height: "300px" }}></p>
+      <h2>3. Start editing to see some magic happen!</h2>
+      <p style={{ height: "300px" }}></p>
+      <h2>4. Start editing to see some magic happen!</h2>
+      <p style={{ height: "300px" }}></p>
+      <h2>5. Start editing to see some magic happen!</h2>
+      <p style={{ height: "300px" }}></p>
+    </div>
+  );
+}
